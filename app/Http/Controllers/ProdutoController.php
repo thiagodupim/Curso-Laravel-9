85d615bcfe7e8309b7ab8ddf8eaf45ab001103ca
Thiago Dupim <thiagodupim3@gmail.com>
2025-07-29 10:02:33 -0300

    Utilizando models

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
@@ -13,7 +14,10 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        return "index";
+        // return "index";
+
+        $produtos = Produto::all();
+        return dd($produtos);
     }
 
     /**
