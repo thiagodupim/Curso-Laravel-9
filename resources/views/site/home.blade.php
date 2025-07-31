c37b0f8fb46877a2200e7b1899cdaafa4308856b
Thiago Dupim <thiagodupim3@gmail.com>
2025-07-31 11:08:46 -0300

    Comentários e operador ternário

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,5 +3,11 @@
 @section('title', 'Essa é a página HOME')
 
 @section('conteudo')
-    <h1> Essa é nossa home </h1>
+    {{-- Isso é um comentário --}}
+
+    {{-- Operador ternário --}}
+    {{-- isset($nome) ? 'existe' : 'não existe' --}}
+
+    {{-- Definindo valor padrão para uma variável --}}
+    {{ $teste ?? 'padrão'}}
 @endsection
