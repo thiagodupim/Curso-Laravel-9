1997d2bb894b33c3c1b7cf8f82d795c7bea65fc1
Thiago Dupim <thiagodupim3@gmail.com>
2025-07-31 16:07:39 -0300

    Estruturas de controle

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -10,4 +10,47 @@
 
     {{-- Definindo valor padrão para uma variável --}}
     {{ $teste ?? 'padrão'}}
+
+    {{-- Estruturas de controle --}}
+
+    @if ($nome == 'Thiago')
+        true
+    @else 
+        false
+    @endif
+
+    {{-- Inverso do if --}}
+    @unless ($nome == 'Thiago')
+        true
+    @else
+        false
+    @endunless
+
+    @switch($idade)
+        @case(25)
+            idade está ok
+            @break
+        @case(28)
+            idade está errada
+            @break
+        @default
+            default
+    @endswitch
+
+    @isset($nome)
+        existe
+    @endisset
+
+    @empty($nome)
+        está vazia
+    @endempty
+
+    @auth 
+        está autenticado
+    @endauth
+
+    {{-- Inverso do auth --}}
+    @guest
+        Não está autenticado
+    @endguest
 @endsection
