6d6f776f41388a39d495acd2c09efa9a79e1c4cc
Thiago Dupim <thiagodupim3@gmail.com>
2025-07-31 09:45:49 -0300

    Criando a factory de produtos e usando StrSlug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             // UsersSeeder::class,
-            CategoriasSeeder::class,
+            // CategoriasSeeder::class,
+            ProdutosSeeder::class,
         ]);
     }
 }
