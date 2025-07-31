2240a13b21cb050397696a80acd24a0a1a809a8a
Thiago Dupim <thiagodupim3@gmail.com>
2025-07-31 17:25:07 -0300

    Usando Stacks e Push

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -11,3 +11,14 @@
         @endslot  
     @endcomponent
 @endsection
+
+@push('style')
+    <<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+@endpush
+
+@push('script')
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+@endpush
+</head>
