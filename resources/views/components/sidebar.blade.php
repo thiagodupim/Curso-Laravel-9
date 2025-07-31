06dd8d53ba75adbab60ad20bc2d3a3c04249c5f8
Thiago Dupim <thiagodupim3@gmail.com>
2025-07-31 17:00:14 -0300

    Usando Include e Component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,4 @@
+<div style="background-color:black; color: #fff">
+    <h1> Sidebar </h1>
+    <p> {{ $paragrafo }} </p>
+</div>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,31 +3,11 @@
 @section('title', 'Essa é a página HOME')
 
 @section('conteudo')
-
-    {{-- Estruturas de repetição --}}
-    @for ($i = 0; $i <= 10; $i++)
-        O valor atual é {{ $i }} <br>
-    @endfor
-
-    <br>
-
-    @php $j = 0; @endphp
-
-    @while ($j <= 15)
-        O valor atual com o while é {{ $j }} <br>
-        @php $j++ @endphp
-    @endwhile
-
-    <br>
-
-    @foreach ($frutas as $fruta)
-        {{ $fruta }} <br>
-    @endforeach
-
-    {{-- Caso o array esteja vázio e vamos trabalhar com um valor padrão usamos forelse --}}
-    @forelse($frutas as $fruta)
-        {{ $fruta }} <br>
-    @empty
-        array está vázio
-    @endforelse
+    @include('includes.mensagem', ['titulo' =>'Mensagem de sucesso!'])
+    
+    @component('components.sidebar')
+        @slot('paragrafo') {{-- Para criar um conteúdo dinâmico para o sidebar --}}
+            Texto qualquer vindo do slot
+        @endslot  
+    @endcomponent
 @endsection
