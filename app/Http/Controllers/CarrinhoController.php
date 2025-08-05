acfd070d09e3827455cc0387f6f945ac13fd78e5
Thiago Dupim <thiagodupim3@gmail.com>
2025-08-05 17:44:56 -0300

    Adicionando itens no carrinho

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -11,4 +11,17 @@ class CarrinhoController extends Controller
         $itens = \Cart::getContent();
         dd($itens);
     }
+
+    public function adicionaCarrinho(Request $request)
+    {
+        \Cart::add([
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->price,
+            'quantity' => $request->qnt,
+            'attributes' => array(
+                'image' => $request->img
+            )
+        ]);
+    }
 }
--- resources/views/site/details.blade.php
+++ resources/views/site/details.blade.php
@@ -16,7 +16,15 @@
             <p>Postado por: {{ $produto->user->firstName }} <br>
             Categoria: {{ $produto->categoria->nome }} 
             </p>
-            <button class="btn-orange btn-large">Comprar</button>
+            <form action="{{ route('site.addcarrinho') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <input type="hidden" name="id" value="{{ $produto->id }}">
+                <input type="hidden" name="name" value="{{ $produto->nome }}">
+                <input type="hidden" name="price" value="{{ $produto->preco }}">
+                <input type="number" name="qnt" value="1">
+                <input type="hidden" name="img" value="{{ $produto->imagem}}">
+                <button class="btn-orange btn-large">Comprar</button>
+            </form>
         </div>
     </div>
 @endsection
